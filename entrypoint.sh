b881a6d0b210089e7f4457499e52349875065336
AJ Bowen <aj@soulshake.net>
2016-12-02 00:26:53 -0800

    Fix URL in 301.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ sed -i "s;__dirname+'/../examples/sample.xml';'$OUR_FILE';" /wopr/server/server.
 
 # Same thing with this line:
 # res.writeHead(301, {'Location': 'https://github.com/yaronn/wopr'});
-OUR_URL="https://github.com/yaronn/wopr"
+OUR_URL="http://blog.soulshake.net/2016/04/command-line-resume/"
 sed -i "s;__dirname+'https://github.com/yaronn/wopr';'$OUR_URL';" /wopr/server/server.js
 
 forever start --debug --verbose  --watch --watchDirectory /data --fifo server.js --port "80"
